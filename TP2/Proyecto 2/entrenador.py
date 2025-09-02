6fb8cf66e1abf0b97f4497dce4b1f7ede94488d4
AgustinCocciardi <agustincocciardi@outlook.com>
2025-09-02 18:48:12 -0300

    dataset y modelo

--- TP2/Proyecto 2/entrenador.py
+++ TP2/Proyecto 2/entrenador.py
@@ -1,10 +1,14 @@
+import os
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import joblib
 
 # Cargar dataset (ajustá la ruta a tu archivo CSV o Excel)
 # Ejemplo: dataset.csv con columnas hu1...hu7 y etiqueta
-df = pd.read_csv("dataset.csv")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "dataset.csv")
+
+df = pd.read_csv(file_path)
 
 # Definir variables independientes (X) y dependiente (y)
 X = df[['hu1', 'hu2', 'hu3', 'hu4', 'hu5', 'hu6', 'hu7']]
